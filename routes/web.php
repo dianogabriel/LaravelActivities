bee3ff1028a23867cc782cf8c3715ebebd5099b2
dianogabriel <78863187+dianogabriel@users.noreply.github.com>
2021-05-05 14:07:28 +0100

    activity 3

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('/posts', 'PostController');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
